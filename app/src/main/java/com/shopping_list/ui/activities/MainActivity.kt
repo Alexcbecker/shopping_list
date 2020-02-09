936d4433d7a681e18f1d8238c9eb07cdc28b62fd
Alexander Cavalheiro Becker <callfrygou@hotmail.com>
2020-02-09 19:20:16 -0200

    Setups navigation | Improves gitignore

--- app/src/main/java/com/shopping_list/ui/activities/MainActivity.kt
+++ app/src/main/java/com/shopping_list/ui/activities/MainActivity.kt
@@ -2,9 +2,15 @@ package com.shopping_list.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.shopping_list.R
 
 class MainActivity : AppCompatActivity() {
+
+    private val _navController: NavController by lazy {
+        Navigation.findNavController(this, R.id.nav_host_fragment)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
